28fb9e03546d1034e2478486fa1d5d9ecdf5cf7b
FelixDiki <118487878+FelixDiki@users.noreply.github.com>
2022-11-17 23:56:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
+#include <iostream>
+
+bool opzione;
 
 int main() {
-   return 0;
-}
+   float a=0;
+    float b = 0;
+   std::cin >> a;
+   std::cin >> b;
+    std::cin >> opzione;
+   switch (opzione)
+
+    {
+        case 0:
+       std:: cout << (a*b)/2<<std::endl;
+       break;
+        case 1:
+            std::cout << a*a <<std::endl;
+            break;
+        case 2:
+            std::cout<< a*b;
+            break;
+            default:
+            std::cout << "opzione non valido"<<std::endl; }
+    }
